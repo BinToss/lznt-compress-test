d56f183e61e9d08611c97b9f6f25581bed0d0fe7
Noah Sherwin <noahrsherwin@gmail.com>
2021-11-09 19:34:52 -0800

    chore: add project files

---
+++ src/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Management;
+
+namespace comtest
+{
+  public static class Program
+  {
+    public static void Main()
+    {
+      try
+      {
+        var target = @"D:\New Folder";
+        var dirInfo = new DirectoryInfo(target);
+        Console.ReadKey();
+        if ((dirInfo.Attributes & FileAttributes.Compressed) != FileAttributes.Compressed)
+        {
+          var objPath = $"Win32_Directory.Name='{target}'";
+          using (var dir = new ManagementObject(objPath))
+          {
+            var outParams = dir.InvokeMethod("Compress", null, null);
+            uint ret = (uint)outParams.Properties["ReturnValue"].Value;
+            Console.WriteLine(dir["Name"]);
+          }
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e);
+      }
+    }
+  }
+}
